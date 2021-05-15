94f7ea668991bb37f22939cc8bfe714d2e14dd28
santiagoismo <santiagoismo@gmail.com>
2021-05-14 23:40:58 -0400

    test commit 2

--- example/src/setupTests.js
+++ example/src/setupTests.js
@@ -2,5 +2,5 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-//this shit was added to do a first commitment
+//this shit was added to do a first commitment XD
 import '@testing-library/jest-dom';
